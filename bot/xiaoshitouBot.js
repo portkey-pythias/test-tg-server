50f190af1d2bf14a5c357773c282b3eb4f702c0e
peter.zhang <pythias@portkey.finance>
2023-11-03 18:20:54 +0800

    部署 机器人服务

--- bot/xiaoshitouBot.js
+++ bot/xiaoshitouBot.js
@@ -1,8 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 const token = "6317366127:AAHPEvgl5k-qfH3uFJ_aQ7slcqDt-vBtZZE";
-// const token = '6914488686:AAG1Fz4mDr2-xIW49x3aob-1BuXRTLRmJCE';
 
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {
   polling: true,
   // request: {
@@ -13,7 +11,6 @@ const bot = new TelegramBot(token, {
 bot.onText(/^\/start$/, (msg, match) => {
   const chatId = msg.chat.id;
 
-  // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, "Welcome everyone to AELF", {
     reply_markup: {
       inline_keyboard: [
@@ -80,7 +77,6 @@ bot.onText(/\/question/, (msg) => {
 });
 
 bot.on("callback_query", (callbackQuery) => {
-  console.log("callbackQuery: ", callbackQuery);
   bot
     .answerCallbackQuery(callbackQuery.id)
     .then(() =>
@@ -91,19 +87,11 @@ bot.on("callback_query", (callbackQuery) => {
 bot.onText(/^\/hello$/, (msg) => {
   const chat = msg.chat;
 
-  console.log("chat: ", chat);
-
   bot.sendMessage(
     chat.id,
     `hello ${chat.last_name} ${chat.first_name}, welcome to xiaoshitou bot`
   );
 });
-
-// bot.on("message", (msg) => {
-//   const chatId = msg.chat.id;
-//   // bot.sendMessage(chatId, "hello xiaoshitou");
-//   // console.log("msg:", msg);
-// });
 
 bot.on("polling_error", (error) => {
   console.log("error", error);
