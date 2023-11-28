05dfd6b3461edfe478922bdd0e510741c45c881c
peter.zhang <pythias@portkey.finance>
2023-11-28 14:27:44 +0800

    test demo

--- server/login.js
+++ server/login.js
@@ -2,19 +2,28 @@ const Router = require("koa-router");
 const router = new Router();
 
 router.get("/auth/callback", async (ctx) => {
-  const queryParameters = ctx.request.query;
-  const { user_id, auth_date, hash } = queryParameters;
-  const botToken = "6317366127:AAHPEvgl5k-qfH3uFJ_aQ7slcqDt-vBtZZE";
-  // const state = req.query.state; // 从回调参数中获取 state
-  const userIdentifier = "generate_user_identifier_based_on_user_id_and_state"; // 生成用户标识
-  // 验证回调参数的有效性
-  const isValid = validateTelegramCallback(user_id, auth_date, hash, botToken);
-  if (isValid) {
-    // 处理用户登录逻辑，比如生成令牌、设置用户会话等
-    const authToken = generateAuthToken(userIdentifier);
+  try {
+    const queryParameters = ctx.request.query;
+    const { user_id, auth_date, hash } = queryParameters;
+    const botToken = "6317366127:AAHPEvgl5k-qfH3uFJ_aQ7slcqDt-vBtZZE";
+    const userIdentifier =
+      "generate_user_identifier_based_on_user_id_and_state"; // 生成用户标识
+    // 验证回调参数的有效性
+    const isValid = validateTelegramCallback(
+      user_id,
+      auth_date,
+      hash,
+      botToken
+    );
 
-    ctx.redirect(`https://test-tg-app.vercel.app/callback?token=${authToken}`);
-  } else {
+    if (isValid) {
+      // 处理用户登录逻辑，比如生成令牌、设置用户会话等
+      const authToken = generateAuthToken(userIdentifier);
+      ctx.redirect(
+        `https://test-tg-app.vercel.app/callback?token=${authToken}`
+      );
+    }
+  } catch (e) {
     // 验证失败，处理错误逻辑
     ctx.status(403).send("Invalid Telegram Login request");
   }
