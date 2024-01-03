2164a38ca3e001cb11f976cdd87e300abc31f9fc
peter.zhang <pythias@portkey.finance>
2024-01-03 16:03:19 +0800

    测试接口

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
 const Router = require("koa-router");
-const router = new Router();
-
 const login = require("./login");
+
+const router = new Router();
 
 router.get("/login/X", async (ctx, next) => {
   try {
-    const requestToken = await login.getRequestToken(ctx);
-    // 保存请求令牌
-    ctx.state.requestToken = requestToken;
-    // 重定向到授权 URL
-    // ctx.redirect(data.oauth_callback);
-    ctx.status = 200;
-    ctx.body = response;
+    ctx.body = 11111;
+    // const requestToken = await login.getRequestToken(ctx);
+    // // 保存请求令牌
+    // ctx.state.requestToken = requestToken;
+    // // 重定向到授权 URL
+    // // ctx.redirect(data.oauth_callback);
+    // ctx.status = 200;
+    // ctx.body = response;
   } catch (e) {
     console.log("error: ", e.toString());
   }
